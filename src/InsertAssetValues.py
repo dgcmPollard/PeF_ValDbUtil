a83e8ccc8683988d2c1c2aedfa4e7155315f02bf
David Pollard <david@pollardsetfilles.com>
2012-08-18 15:13:33 +0100

    Split db routines into separate module ValDbUtils

--- src/InsertAssetValues.py
+++ src/InsertAssetValues.py
@@ -8,83 +8,7 @@ owned by Pollards Et Filles Ltd.
 @author: dpollard2
 '''
 
-
-def connectMysql(dbname):
-    '''Make and return connection to Mysql dbase dbname '''
-    import sys
-    import MySQLdb
-    
-    try:
-        conn = MySQLdb.connect (db = dbname,
-                                host = "localhost",
-                                port = 3307,
-                                user = "dpollard2",
-                                passwd = "connect2mysql")
-    except Exception, e:
-        print "Cannot connect to server! %s. Exiting" % e
-        sys.exit()
-    else:
-        print "Connected to %s" % dbname
-        return conn
-
-
-def getAssets(dbname):
-    '''Get list of assets, ids, close times and timezones and return as dict
-    keyed on tuple (exchangeID, assetID) '''
-    
-    connA = connectMysql(dbname)
-    asst = dict()
-    csr = connA.cursor()
-    try:
-        csr.execute("""select e.exchangeID, a.ticker, a.assetID, a.close, e.timezone
-        from asset a, exchange e 
-        where a.exchangeID = e.exchangeID
-        order by e.exchangeID, a.ticker""")
-#        print "No. rows selected: %d" % csr.rowcount
-        rows = csr.fetchall()
-        for r in rows:
-            asst[(int(r[0]),r[1])] = (int(r[2]), r[3], r[4])
-    except Exception, e:
-        print "Error during asset list selection: %s" % e
-    finally:
-        csr.close()
-#        print "Asset dictionary: ", asst  #print dictionary
-        return asst
-
-
-def getExchanges(dbname):
-    '''Get list of exchangeIDs return as dict keyed on exchange(name)'''
-    
-    connA = connectMysql(dbname)
-    exch = dict()
-    csr = connA.cursor()
-    try:
-        csr.execute("""select exchange, exchangeID from exchange order by exchangeID""")
-#        print "No. rows selected: %d" % csr.rowcount
-        rows = csr.fetchall()
-        for r in rows:
-            exch[r[0]] = int(r[1])
-    except Exception, e:
-        print "Error during exchange list selection: %s" % e
-    finally:
-        csr.close()
-#        print "Exchange dictionary: ", exch  #print dictionary
-        return exch
-    
-    
-def reformatDate( ddmmmyy, hhmmss, tzone):
-    ''' change date format from dd-mmm-yy to yyyy-mm-dd hh:mm:ss converted to UTC'''
-    import datetime
-    import pytz
-    dt= datetime.datetime.strptime(ddmmmyy + " " + hhmmss,'%d-%b-%y %H:%M:%S')
-#    utc = pytz.utc
-    locTZ = pytz.timezone(tzone)
-    locDT = locTZ.localize(dt)
-    utcDT = locDT.astimezone(pytz.utc)
-    return utcDT.strftime('%Y-%m-%d %H:%M')
-
-
-
+import ValDbUtils as utl
 
 if __name__ == '__main__':
     ''' Read CSV files for price data on asset and insert into (Test)ValDb
@@ -105,8 +29,8 @@ if __name__ == '__main__':
     args= pars.parse_args()
 
     try:
-        asst= getAssets( args.dbase)
-        exchange = getExchanges( args.dbase)
+        asst= utl.getAssets( args.dbase)
+        exchange = utl.getExchanges( args.dbase)
         exID = exchange[(args.exch).upper()]
     except Exception, e:
         sys.exit('Exiting %s...Problem with Assets or Exchanges; %s' % (sys.argv[0], e))
@@ -118,7 +42,7 @@ if __name__ == '__main__':
         sys.exit("Exiting ... File %s not found" % filename)
         
 #    connect to database
-    conn = connectMysql(args.dbase)
+    conn = utl.connectMysql(args.dbase)
     csr = conn.cursor()
 
 #    read csv file of prices and convert to list
@@ -132,7 +56,7 @@ if __name__ == '__main__':
                     if ((row['Price'].find('N/A') == -1) and (len(row['Price'])>0)):
                         hhmmss = str( asst[(exID,row['Ticker'])][1] )
                         tzon = asst[(exID,row['Ticker'])][2]
-                        sqlStmt= "insert into value (assetID, datetime, valueTypeID, value) values (%d, '%s', 1, %s)" % (asst[(exID,row['Ticker'])][0], reformatDate(row['Date'],hhmmss,tzon), row['Price'])
+                        sqlStmt= "insert into value (assetID, datetime, valueTypeID, value) values (%d, '%s', 1, %s)" % (asst[(exID,row['Ticker'])][0], utl.reformatDate(row['Date'],hhmmss,tzon), row['Price'])
                         print sqlStmt
                         csr.execute(sqlStmt)
                         numInserted += 1
---
+++ src/ValDbUtils.py
@@ -0,0 +1,82 @@
+'''
+Created on Aug 18, 2012
+Python module with routines for PeF_ValDb access and
+data management.
+Owned by Pollards Et Filles Ltd.
+@author: dpollard2
+'''
+
+
+def connectMysql(dbname):
+    '''Make and return connection to Mysql dbase dbname '''
+    import sys
+    import MySQLdb
+    
+    try:
+        conn = MySQLdb.connect (db = dbname,
+                                host = "localhost",
+                                port = 3307,
+                                user = "dpollard2",
+                                passwd = "connect2mysql")
+    except Exception, e:
+        print "Cannot connect to server! %s. Exiting" % e
+        sys.exit()
+    else:
+        print "Connected to %s" % dbname
+        return conn
+
+
+def getAssets(dbname):
+    '''Get list of assets, ids, close times and timezones and return as dict
+    keyed on tuple (exchangeID, assetID) '''
+    
+    connA = connectMysql(dbname)
+    asst = dict()
+    csr = connA.cursor()
+    try:
+        csr.execute("""select e.exchangeID, a.ticker, a.assetID, a.close, e.timezone
+        from asset a, exchange e 
+        where a.exchangeID = e.exchangeID
+        order by e.exchangeID, a.ticker""")
+#        print "No. rows selected: %d" % csr.rowcount
+        rows = csr.fetchall()
+        for r in rows:
+            asst[(int(r[0]),r[1])] = (int(r[2]), r[3], r[4])
+    except Exception, e:
+        print "Error during asset list selection: %s" % e
+    finally:
+        csr.close()
+#        print "Asset dictionary: ", asst  #print dictionary
+        return asst
+
+
+def getExchanges(dbname):
+    '''Get list of exchangeIDs return as dict keyed on exchange(name)'''
+    
+    connA = connectMysql(dbname)
+    exch = dict()
+    csr = connA.cursor()
+    try:
+        csr.execute("""select exchange, exchangeID from exchange order by exchangeID""")
+#        print "No. rows selected: %d" % csr.rowcount
+        rows = csr.fetchall()
+        for r in rows:
+            exch[r[0]] = int(r[1])
+    except Exception, e:
+        print "Error during exchange list selection: %s" % e
+    finally:
+        csr.close()
+#        print "Exchange dictionary: ", exch  #print dictionary
+        return exch
+    
+    
+def reformatDate( ddmmmyy, hhmmss, tzone):
+    ''' change date format from dd-mmm-yy to yyyy-mm-dd hh:mm:ss converted to UTC'''
+    import datetime
+    import pytz
+    dt= datetime.datetime.strptime(ddmmmyy + " " + hhmmss,'%d-%b-%y %H:%M:%S')
+#    utc = pytz.utc
+    locTZ = pytz.timezone(tzone)
+    locDT = locTZ.localize(dt)
+    utcDT = locDT.astimezone(pytz.utc)
+    return utcDT.strftime('%Y-%m-%d %H:%M')
